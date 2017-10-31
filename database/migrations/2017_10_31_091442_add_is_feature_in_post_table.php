ab014332aa6ea4cb253f7cac1b9a0aff2fa55b18
SKILL573 <a9101021249@gmail.com>
2017-10-31 17:21:36 +0800

    練習七 commander 表示這兩行會報錯 $table‐>string('title',500)‐>change(); $table‐>boolean('is_feature')‐>default(false)‐>after('content');

--- database/migrations/2017_10_31_091442_add_is_feature_in_post_table.php
+++ database/migrations/2017_10_31_091442_add_is_feature_in_post_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Migration1 extends Migration
+class AddIsFeatureInPostTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,8 @@ class Migration1 extends Migration
      */
     public function up()
     {
-        //
+        table -> string('title',500)-> change();
+        table -> boolean('is_feature')-> default(false)-> after('content');
     }
 
     /**
